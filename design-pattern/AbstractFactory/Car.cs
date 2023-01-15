7011e7c49bda875a18206f0e61719f8c5c417b9d
chicken <hibary.taiwan@gmail.com>
2023-01-15 18:24:32 +0800

    init

---
+++ design-pattern/AbstractFactory/Car.cs
@@ -0,0 +1,24 @@
+﻿namespace design_pattern.AbstractFactory
+{
+    internal interface ICar
+    {
+        string GetCarType();
+    }
+
+    class SportsCar : ICar
+    {
+        public string GetCarType()
+        {
+            return "Sports Car";
+        }
+    }
+
+    class FamilyCar : ICar
+    {
+        public string GetCarType()
+        {
+            return "Family Car";
+        }
+    }
+
+}
