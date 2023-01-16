90861a3ee26faa2e48540cb1b00475fb623b4ed6
chicken <hibary.taiwan@gmail.com>
2023-01-16 23:39:08 +0800

    feat: 創造者模式

---
+++ design-pattern/Builder/ConcreteBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace design_pattern.Builder
+{
+    internal abstract class Builder
+    {
+        public abstract void BuildDoor();
+        public abstract void BuildWall();
+        public abstract void BuildWindow();
+        public abstract House GetProduct();
+    }
+
+    internal class ConcreteBuilder : Builder
+    {
+        private House _product = new();
+
+        public override void BuildDoor()
+        {
+            _product.Door = "門";
+        }
+
+        public override void BuildWall()
+        {
+            _product.Wall = "牆壁";
+        }
+
+        public override void BuildWindow()
+        {
+            _product.Window = "窗戶";
+        }
+
+        public override House GetProduct()
+        {
+            return _product;
+        }
+    }
+}
---
+++ design-pattern/Builder/Director.cs
@@ -0,0 +1,12 @@
+﻿namespace design_pattern.Builder
+{
+    internal class Director
+    {
+        public static void Construct(Builder builder)
+        {
+            builder.BuildDoor();
+            builder.BuildWall();
+            builder.BuildWindow();
+        }
+    }
+}
