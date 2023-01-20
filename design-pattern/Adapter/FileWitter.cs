e42739181be2ea0b9cd9b55761e7cd8490d533af
chicken <hibary.taiwan@gmail.com>
2023-01-21 02:34:13 +0800

    feat: adapter pattern

---
+++ design-pattern/Adapter/FileWitter.cs
@@ -0,0 +1,24 @@
+﻿namespace design_pattern.Adapter
+{
+
+    internal interface IFileWitter
+    {
+        void Write();
+    }
+
+    internal class JsonFileWitter : IFileWitter
+    {
+        public void Write()
+        {
+            Console.WriteLine("File.json");
+        }
+    }
+
+    internal class CsvFileWitter : IFileWitter
+    {
+        public void Write()
+        {
+            Console.WriteLine("File.csv");
+        }
+    }
+}
